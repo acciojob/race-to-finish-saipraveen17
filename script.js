59843618810718e70abd55936ad36f4b4c72dddd
Praveen Katikireddy <praveenk9417@gmail.com>
2023-04-24 10:56:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-window.promises = [];
+window.promises = [
+  new Promise(resolve => setTimeout(() => resolve(1), Math.random() * 5000)),
+  new Promise(resolve => setTimeout(() => resolve(2), Math.random() * 5000)),
+  new Promise(resolve => setTimeout(() => resolve(3), Math.random() * 5000)),
+  new Promise(resolve => setTimeout(() => resolve(4), Math.random() * 5000)),
+  new Promise(resolve => setTimeout(() => resolve(5), Math.random() * 5000))
+];
+
+Promise.any(promises)
+  .then(result => {
+    const outputDiv = document.getElementById('output');
+    outputDiv.innerHTML = `First promise to resolve: ${result}`;
+  });
 
 // Do not change the code above this
 // add your promises to the array `promises`
